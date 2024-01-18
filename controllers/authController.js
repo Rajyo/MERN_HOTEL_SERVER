877bd1aa7aed4b3dc4089fc8d197bcee466dd3f6
Rajyo <yopro1999@gmail.com>
2024-01-18 18:01:14 +0530

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,81 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+//register function
+export const register = async(req,res,next) =>{
+    const user = await User.findOne({email:req.body.email});
+    if(user) return next(createError(404, "User already exists"));
+    
+
+    try{
+        //user password hashing - I am using bcryptjs. https://www.npmjs.com/package/bcryptjs
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const newUser = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hash
+        })
+
+        await newUser.save();
+        res.status(200).send("User has been created.");
+    }catch(err){
+        next(err);
+    }
+}
+
+
+//login function
+
+export const login = async(req,res,next) =>{
+    try{
+        const user = await User.findOne({email:req.body.email});
+        if(!user) return next(createError(404, "User not found!"));
+
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password 
+        );
+
+
+        if(!isPasswordCorrect)
+         return next(createError(400, "Wrong password or username"));
+
+        // jsonwebtoken was used
+        const token = jwt.sign({id:user._id, isAdmin:user.isAdmin}, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        //setting this token into our cookies. first install cookie parser
+
+        const {_id,...otherDetails} = user._doc;
+        // res
+        //   .cookie("access_token", token,
+        // //   {maxAge: 60 * 60 * 1000, // 1 hour
+        // //   sameSite: "none",
+        // //   secure: isCookieSecure,
+        // //   httpOnly: isCookieSecure}
+        //   )
+        //   .status(200)
+        //   .json({_id});
+        res.status(200).json(({ _id, token }))
+    }catch(err){
+        console.log(err)
+        next(err);
+    }
+}
+
+
+//logout function
+export const logout = async(req,res,next) =>{
+    try{
+        res
+          .clearCookie("access_token")
+          .status(200)
+          .json({data: "Cookie Deleted"});
+    }catch(err){
+        console.log(err)
+        next(err);
+    }
+}
---
+++ utils/verifyToken.js
@@ -0,0 +1,87 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+
+//verify user function
+export const verifyUser = (req, res, next) => {
+    if (!req.headers.authorization) return res.status(403).json({ msg: "Not authorized. No token" })
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+        const token = req.headers.authorization.split(" ")[1]
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) return next(createError(403, "Token is not valid"))
+            req.user = user;
+            //console.log("verifyUser",req.user)
+            if (req.user.id || req.user.isAdmin) {
+                next();
+            } else {
+                return next(createError(403, "You are not authorized"))
+            }
+        })
+    }
+
+}
+
+
+//verify admin function
+export const verifyAdmin = (req, res, next) => {
+    if (!req.headers.authorization) return res.status(403).json({ msg: "Not authorized. No token" })
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+        const token = req.headers.authorization.split(" ")[1]
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) return next(createError(403, "Token is not valid"))
+            req.user = user;
+            //console.log("verifyAdmin",req.user)
+            if (req.user.isAdmin) {
+                next();
+            } else {
+                return next(createError(403, "You are not authorized"))
+            }
+        })
+    }
+
+}
+
+
+
+// //verify user function
+// export const verifyUser = (req, res, next) => {
+//     const token = req.cookies.access_token;
+//     if (!token) {
+//         return next(createError(401, "You are not authenticated!"))
+//     }
+
+//     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//         if (err) return next(createError(403, "Token is not valid"))
+//         req.user = user;
+//         //console.log("verifyUser",req.user)
+//         if (req.user.id === req.params.id || req.user.isAdmin) {
+//             next();
+//         } else {
+//             return next(createError(403, "You are not authorized"))
+//         }
+//     })
+// }
+
+
+// //verify admin function
+// export const verifyAdmin = (req, res, next) => {
+//     const token = req.cookies.access_token;
+//     if (!token) {
+//         return next(createError(401, "You are not authenticated!"))
+//     }
+
+//     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//         if (err) return next(createError(403, "Token is not valid"))
+//         req.user = user;
+//         //console.log("verifyAdmin",req.user)
+//         if (req.user.isAdmin) {
+//             next();
+//         } else {
+//             return next(createError(403, "You are not authorized"))
+//         }
+//     })
+
+// }
