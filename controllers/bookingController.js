2c56848ee9874772b851c221fb0f242e86500abb
Rajyo <yopro1999@gmail.com>
2024-05-01 21:52:26 +0530

    resolved user booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -32,19 +32,24 @@ export const checkoutBooking = async (req, res, next) => {
             consent_collection: {
                 terms_of_service: 'required',
             },
+
             custom_text: {
+                shipping_address: {
+                    message: 'CARD INFORMATION: CARD NUMBER = "4242 4242 4242"..........MM/YY = "Add any month and year of the future"..........CVC = "Any 3 digit number"'
+                },
                 terms_of_service_acceptance: {
                     message: 'I agree to the [Terms of Service](https://example.com/terms)',
                 },
+
             },
 
         });
         //console.log("session", session)
-        if(session){
-            const newBooking = new Booking({bookingId:session.id , ...req.body})
+        if (session) {
+            const newBooking = new Booking({ bookingId: session.id, ...req.body })
             try {
                 const savedBooking = await newBooking.save()
-                await User.findByIdAndUpdate(req.user.id, {$push: {bookings: savedBooking._id}})
+                await User.findByIdAndUpdate(req.user.id, { $push: { bookings: savedBooking._id } })
             } catch (error) {
                 console.log(error)
             }
@@ -59,16 +64,16 @@ export const checkoutBooking = async (req, res, next) => {
 
 export const createBooking = async (req, res, next) => {
     try {
-      const session = await stripe.checkout.sessions.retrieve(req.params.id)
-      //console.log("session", session.id, session.shipping_details.address, session.status)
+        const session = await stripe.checkout.sessions.retrieve(req.params.id)
+        //console.log("session", session.id, session.shipping_details.address, session.status)
 
-      try {
-        await Booking.updateOne({bookingId: session.id}, { $set: { status: session.status, address: session.shipping_details.address } },{ upsert: true })        
-        
-        res.status(200).json(session.id)
-      } catch (error) {
-        console.log(error)
-      }
+        try {
+            await Booking.updateOne({ bookingId: session.id }, { $set: { status: session.status, address: session.shipping_details.address } }, { upsert: true })
+
+            res.status(200).json(session.id)
+        } catch (error) {
+            console.log(error)
+        }
     } catch (error) {
         console.log(error)
         res.status(404).json(error.raw.message)
@@ -77,19 +82,19 @@ export const createBooking = async (req, res, next) => {
 
 export const cancelBooking = async (req, res, next) => {
     try {
-      const session = await stripe.checkout.sessions.retrieve(req.params.id)
+        const session = await stripe.checkout.sessions.retrieve(req.params.id)
 
-      try {
-        
-        const deletedBooking = await Booking.findOneAndDelete({bookingId: session.id})
-        await User.findByIdAndUpdate(req.user.id, {$pull: {bookings: deletedBooking._id}})
+        try {
 
-        const expireSession = await stripe.checkout.sessions.expire(req.params.id);
-        res.status(200).json(expireSession.id)
-    } catch (error) {
-        console.log(error)   
-    }
-      
+            const deletedBooking = await Booking.findOneAndDelete({ bookingId: session.id })
+            await User.findByIdAndUpdate(req.user.id, { $pull: { bookings: deletedBooking._id } })
+
+            const expireSession = await stripe.checkout.sessions.expire(req.params.id);
+            res.status(200).json(expireSession.id)
+        } catch (error) {
+            console.log(error)
+        }
+
     } catch (error) {
         await stripe.checkout.sessions.expire(req.params.id);
         console.log(error)
@@ -98,9 +103,9 @@ export const cancelBooking = async (req, res, next) => {
 }
 
 
-export const getBooking =async (req, res, next) => {
+export const getBooking = async (req, res, next) => {
     try {
-        const getBookings = await Booking.findOne({bookingId: req.params.id}).populate("roomId")
+        const getBookings = await Booking.findOne({ bookingId: req.params.id }).populate("roomId")
         res.status(200).json(getBookings)
     } catch (error) {
         console.log(error)
@@ -108,7 +113,7 @@ export const getBooking =async (req, res, next) => {
 }
 
 
-export const getAllBooking =async (req, res, next) => {
+export const getAllBooking = async (req, res, next) => {
     try {
         const getAllBookings = await Booking.find({})
         res.status(200).json(getAllBookings)
@@ -120,7 +125,16 @@ export const getAllBooking =async (req, res, next) => {
 
 export const getUserBookings = async (req, res, next) => {
     try {
-        const user = await User.findById({_id: req.user.id}).populate("bookings").select("-password")
+        let user = await User.findById({ _id: req.user.id }).populate("bookings").select("-password")
+        if (user.bookings.length > 0) {
+            user.bookings.map(async (item) => {
+                if (item.status == "open") {
+                    console.log("status open", item._id)
+                    await Booking.findByIdAndDelete({ _id: item._id })
+                    await User.findByIdAndUpdate(req.user.id, { $pull: { bookings: item._id } })
+                }
+            })
+        }
         res.status(200).json(user)
     } catch (error) {
         console.log(error)
@@ -128,26 +142,26 @@ export const getUserBookings = async (req, res, next) => {
 }
 
 
-export const removeOpenStatusBookings = async(req, res, next) => {
-    const user = await User.findById({_id: req.user.id}).populate("bookings").select("-password")
-    user.bookings.map(async(item) => {
-        if(item.status == "open"){
-            console.log("status open",item._id)
-            await Booking.findByIdAndDelete({_id: item._id})
-            await User.findByIdAndUpdate(req.user.id, {$pull: {_id: item._id}})
+export const removeOpenStatusBookings = async (req, res, next) => {
+    const user = await User.findById({ _id: req.user.id }).populate("bookings").select("-password")
+    user.bookings.map(async (item) => {
+        if (item.status == "open") {
+            console.log("status open", item._id)
+            await Booking.findByIdAndDelete({ _id: item._id })
+            await User.findByIdAndUpdate(req.user.id, { $pull: { bookings: item._id } })
         }
     })
     res.status(200).json("Removed booking with open status")
 }
 
 
-export const removeCancelThenSuccessBookings = async(req, res, next) => {
+export const removeCancelThenSuccessBookings = async (req, res, next) => {
     try {
         const bookings = await Booking.find({})
-        bookings.map(async(it) => {
-            if(!it.adults){
+        bookings.map(async (it) => {
+            if (!it.adults) {
                 console.log(it._id)
-                await Booking.findByIdAndDelete({_id: it._id})
+                await Booking.findByIdAndDelete({ _id: it._id })
             }
         })
         res.status(200).json("Removed Cancel then Success Bookings")
